887f4383602e330b5a5dbfc755f8065db03a84ce
Vahe <bazikyan321@icloud.com>
2020-10-13 19:25:10 +0300

    Initial Commit

---
+++ TestGenesis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGenesis
+//
+//  Created by Ваге Базикян on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
